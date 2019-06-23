286526b1e75d776e5059881eccd77060625ca6e8
lloyd <lloydsongxie@gmail.com>
2019-06-23 10:31:34 -0700

    WIP: Add typing sounds

--- app/services/AudioService.js
+++ app/services/AudioService.js
@@ -1,8 +1,15 @@
 import { Audio } from 'expo';
 
 export default class AudioService {
-    load = async () => {
-        this.soundObject = new Expo.Audio.Sound();
+    audioFilesMap = {
+        'BG_1': 'bg_1.mp3',
+        'TYPE_1': 'type_1.mp3'
+    }
+
+    soundObjects = {}
+
+    load = async (objectName, fileName) => {
+        const soundObject = new Expo.Audio.Sound();
 
         initialStatus = {
             progressUpdateIntervalMillis: 500,
@@ -26,19 +33,37 @@ export default class AudioService {
                 playThroughEarpieceAndroid: false
             });
             await this.soundObject.loadAsync(require('../assets/bg/bg_1.mp3'));
-            await this.soundObject.setIsLoopingAsync(true)
+            // await this.soundObject.setIsLoopingAsync(true)
         } catch (error) {
             console.log(error)
         }
+
+        return soundObject
+    }
+    
+    loadFiles() {
+        foreach (val, key audioFilesMap) {
+            objectName = key; 
+            fileName = val;
+            soundObject = load(, )
+            this.soundObjects[objectName] = soundObject;
+        }
+        /**
+         * To play the exact sound, you will use AudioService.play(soundObject)
+         * TODO:
+         * 1) first test that the object returned has the callable play()
+         * 2) call play() from the typewriter library for ONE sound file to test it out
+         * 3) Finish implementing the map looping solution so it works for multiple sounds
+         */
     }
 
     // play the background music!
-    play = async () => {
-        await this.soundObject.setPositionAsync(0);
-        await this.soundObject.playAsync();
+    play = async (soundObject) => {
+        await soundObject.setPositionAsync(0);
+        await soundObject.playAsync();
     }
 
-    unmount = async () => {
-        await this.soundObject.stopAsync();
+    unmount = async (soundObject) => {
+        await soundObject.stopAsync();
     }
 }
